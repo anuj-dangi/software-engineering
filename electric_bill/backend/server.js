cb71330d5acb8d17686efe236a95cba8fc8f426f
Anuj Dangi <23mcce16@uohyd.ac.in>
2026-01-20 11:18:26 +0530

    fixed cors error

--- electric_bill/backend/server.js
+++ electric_bill/backend/server.js
@@ -5,7 +5,7 @@ const DataModel = require('./models/DataModel');
 
 const app = express();
 app.use(cors(
-    {origin:'http://localhost:5500'}
+    {origin:'http://localhost:5500/*'}
 ));
 
 app.use(express.json());
